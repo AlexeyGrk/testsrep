6e57bbfc3896964c5691a8aaee8a35b5920dc40b
AlexeyGrk <alexgmirko@gmail.com>
2021-03-24 11:06:54 +0200

    добавляет интеренсные примеры

---
+++ js-1.js
@@ -0,0 +1,43 @@
+///// switch case /////
+// const stars = 1;
+// let price;
+
+// switch (stars) {
+//   case 1:
+//   case 4:
+//     price = 50;
+//     break;
+//   case 2:
+//     price = 70;
+//     break;
+//   case 3:
+//     price = 800;
+//     break;
+//   default:
+//     console.log("Алексей - телепузик");
+// }
+// console.log(price);
+///// switch case /////
+
+///// for /////
+
+// const min = 1;
+// const max = 10;
+// let total = 0;
+// for (let i = min; i < max; i += 1) {
+//   if (i % 2 !== 0) {
+//     console.log("NOчётное", i);
+//     continue;
+//   }
+//   console.log("чётное", i);
+//   total += i;
+// }
+// console.log(total);
+///// for /////
+
+///// интересности /////
+
+// console.log(typeof NaN); // Number
+// console.log(typeof typeof NaN); // String
+
+console.log("mango".indexOf("nam"));
