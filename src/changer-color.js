ed8d206fba29b396f084feecdc47ac271c204f2b
AlexeyGrk <alexgmirko@gmail.com>
2021-05-02 19:23:28 +0300

    add correct changer-color

--- src/changer-color.js
+++ src/changer-color.js
@@ -15,16 +15,25 @@ const refs = {
   stopBtn: document.querySelector("button[data-action=stop]"),
 };
 let intervalColor = undefined;
+let isActive = false;
 
 refs.startBtn.addEventListener("click", changeBodyRandomColor);
 refs.stopBtn.addEventListener("click", clearIntervalColor);
+
 function changeBodyRandomColor() {
+  if (isActive) {
+    return;
+  }
   intervalColor = setInterval(() => {
     const randomColor = colors[randomIntegerFromInterval(0, colors.length - 1)];
     //   console.log(randomColor);
     document.body.style.backgroundColor = randomColor;
+    isActive = true;
+    refs.startBtn.disabled = true;
   }, 1000);
 }
 function clearIntervalColor() {
+  isActive = false;
+  refs.startBtn.disabled = false;
   clearInterval(intervalColor);
 }
