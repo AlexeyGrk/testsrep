d0565c8c8afbac37dd5754854697e70aec3b3964
AlexeyGrk <alexgmirko@gmail.com>
2021-05-10 22:14:39 +0300

    add draft hw

--- src/http-try.js
+++ src/http-try.js
@@ -1,18 +1,28 @@
 import debounce from "lodash.debounce";
 const refs = {
   inputnRef: document.querySelector(".input-country-js"),
+  searchForm: document.querySelector(".js-searchForm"),
+  cardList: document.querySelector("js-cardLand"),
 };
 
-function fetchCountry(countryName) {
-  return fetch(`https://restcountries.eu/rest/v2/name/${countryName}`).then(
+function fetchCountry(searchQuery) {
+  return fetch(`https://restcountries.eu/rest/v2/name/${searchQuery}`).then(
     (r) => {
-      return [r.json()];
+      return r.json();
     }
   );
 }
+
 refs.inputnRef.addEventListener(
   "input",
   debounce((e) => {
     console.log(refs.inputnRef.value);
   }, 500)
 );
+function findCountry(event) {
+  event.preventDefault();
+  const searchQuery = refs.searchForm.elements.query.value;
+  fetchCountry(searchQuery).then((country) => {
+    console.log(...country);
+  });
+}
